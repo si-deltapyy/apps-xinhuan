b21159befc109faa76f562b036eea23464bba411
si-deltapyy <sguritno16@student.uns.ac.id>
2024-11-26 09:46:59 +0700

    add capacitor configuration file for the project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.xinhuangsr.app',
+  appName: 'Xinhuan GSR',
+  webDir: 'www'
+};
+
+export default config;
